d6d1ef0255e4f9063c4807367783d65a0186c628
fabiossussin <fabio@xplay.digital>
2022-06-21 13:21:48 -0300

    finalizado ajustes

--- Business/API/Intra/Game/BlGame.cs
+++ Business/API/Intra/Game/BlGame.cs
@@ -1,6 +1,6 @@
 ﻿using DAO.DBConnection;
+using DAO.Intra.Employee;
 using DAO.Intra.Game;
-using DAO.Intra.UserDAO;
 using DTO.General.Base.Api.Output;
 using DTO.Intra.Game.Database;
 using System.Linq;
@@ -10,21 +10,21 @@ namespace Business.API.Intra.Game
 {
     public class BlGame
     {
-        private readonly IntraUserDAO IntraUserDAO;
         private readonly IntraGameDAO IntraGameDAO;
+        private readonly IntraEmployeeDAO IntraEmployeeDAO;
 
         public BlGame(XDataDatabaseSettings settings)
         {
-            IntraUserDAO = new(settings);
+            IntraEmployeeDAO = new(settings);
             IntraGameDAO = new(settings);
         }
 
         public BaseApiOutput FinishGame(IntraGame game)
         {
-            if (IntraUserDAO.FindById(game.PlayerOneId) == null)
+            if (IntraEmployeeDAO.FindById(game.PlayerOneId) == null)
                 return new("Jogador Um não encontrado!");
 
-            if (IntraUserDAO.FindById(game.PlayerTwoId) == null)
+            if (IntraEmployeeDAO.FindById(game.PlayerTwoId) == null)
                 return new("Jogador Dois não encontrado!");
 
             if (!(game?.Sets?.Any() ?? false))
@@ -41,7 +41,7 @@ namespace Business.API.Intra.Game
                 if (set.PlayerOnePoints < 21 && set.PlayerTwoPoints < 21)
                     return new($"Set {set.SetNumber}, não foi finalizado!");
 
-                if (NumberExtension.GetDiff(set.PlayerOnePoints, set.PlayerTwoPoints) != 2)
+                if (NumberExtension.GetDiff(set.PlayerOnePoints, set.PlayerTwoPoints) < 2)
                     return new($"A diferença de Pontos está diferente de 2 no Set {set.SetNumber}!");
             }
 
--- Business/API/Intra/Menu/BlIntraMenu.cs
+++ Business/API/Intra/Menu/BlIntraMenu.cs
@@ -21,7 +21,7 @@ namespace Business.API.Intra.Menu
             });
 
             menus.Add(new("Dashboard", new("dashboard", IntraIconTypeEnum.Material), "home"));
-            menus.Add(new("Novo Jogo", new("fas fa - shuttlecock", IntraIconTypeEnum.FontAwesome), "game/new"));
+            menus.Add(new("Novo Jogo", new("fas fa - shuttlecock", IntraIconTypeEnum.FontAwesome), "game"));
             menus.Add(registers);
 
             return menus;
